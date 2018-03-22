aeb12f8677d03cf0537ea11dabfb42298708f2db
Grant Cohoe <grant@grantcohoe.com>
2018-03-22 19:02:42 -0400

    prototype of power

--- manifests/profiles/windows/power.pp
+++ manifests/profiles/windows/power.pp
@@ -25,23 +25,44 @@ class profiles::windows::power {
     $display_subgroup_guid = '7516b95f-f776-4464-8c53-06167f40cc99'
     $display_sleep_guid = '3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e'
 
-    registry_value { 'DisplaySleepTime':
-        path => "HKLM\\SYSTEM\\CurrentControlSet\\Control\\Power\\User\\PowerSchemes\\${power_scheme_guid}\\${
-            display_subgroup_guid}\\${display_sleep_guid}\ACSettingIndex",
-        type => 'dword',
-        data => 0
-    }
+    # registry_value { 'DisplaySleepTime':
+    #     path => "HKLM\\SYSTEM\\CurrentControlSet\\Control\\Power\\User\\PowerSchemes\\${power_scheme_guid}\\${
+    #         display_subgroup_guid}\\${display_sleep_guid}\ACSettingIndex",
+    #     type => 'dword',
+    #     data => 0
+    # }
+    #
+    # reg_acl { 'DisplaySleepTime':
+    #     target      => "HKLM\\SYSTEM\\CurrentControlSet\\Control\\Power\\User\\PowerSchemes\\${power_scheme_guid}\\${
+    #         display_subgroup_guid}\\${display_sleep_guid}",
+    #     owner       => 'BUILTIN\Administrators',
+    #     permissions => [
+    #         { 'RegistryRights' => 'FullControl', 'IdentityReference' => 'BUILTIN\Administrators' }
+    #     ]
+    # }
+    #
+    # Reg_acl['DisplaySleepTime'] -> Registry_value['DisplaySleepTime']
 
-    reg_acl { 'DisplaySleepTime':
-        target      => "HKLM\\SYSTEM\\CurrentControlSet\\Control\\Power\\User\\PowerSchemes\\${power_scheme_guid}\\${
-            display_subgroup_guid}\\${display_sleep_guid}",
-        owner       => 'BUILTIN\Administrators',
-        permissions => [
-            { 'RegistryRights' => 'FullControl', 'IdentityReference' => 'BUILTIN\Administrators' }
-        ]
-    }
+    $sleep_subgroup_guid = '238c9fa8-0aad-41ed-83f4-97be242c8f20'
+    $sleep_after_guid = '29f6c1db-86da-48c5-9fdb-f2b67b1f44da'
 
-    Reg_acl['DisplaySleepTime'] -> Registry_value['DisplaySleepTime']
+    # registry_value { 'ACSettingIndex':
+    #     path => "HKLM\\SYSTEM\\CurrentControlSet\\Control\\Power\\User\\PowerSchemes\\${power_scheme_guid}\\${
+    #         sleep_subgroup_guid}\\${sleep_after_guid}",
+    #     type => 'dword',
+    #     data => 0
+    # }
+
+    # reg_acl { 'SystemSleepTime':
+    #     target      => "HKLM\\SYSTEM\\CurrentControlSet\\Control\\Power\\User\\PowerSchemes\\${power_scheme_guid}\\${
+    #         sleep_subgroup_guid}\\${sleep_after_guid}",
+    #     owner       => 'BUILTIN\Administrators',
+    #     permissions => [
+    #         { 'RegistryRights' => 'FullControl', 'IdentityReference' => 'BUILTIN\Administrators' }
+    #     ]
+    # }
+
+    #Reg_acl['SystemSleepTime'] -> Registry_value['SystemSleepTime']
 
     # Administrators need rights to HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes
 
@@ -50,4 +71,34 @@ class profiles::windows::power {
 
     # setacl.exe -on "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\381b4222-f694-41f0-9685-ff5bb260df2e\7516b95f-f776-4464-8c53-06167f40cc99" -ot reg -actn setowner -ownr "n:Administrators" -rec yes
     # https://forge.puppet.com/ipcrm/registry_acl/0.0.1
+
+    reg_acl { 'CustomSettings':
+        target      => "HKLM\\SYSTEM\\CurrentControlSet\\Control\\Power\\User\\PowerSchemes\\${power_scheme_guid}",
+        owner       => 'BUILTIN\Administrators',
+        permissions => [
+            { 'RegistryRights' => 'FullControl', 'IdentityReference' => 'BUILTIN\Administrators' }
+        ]
+    }
+
+    # registry_key { 'DisplaySleepSubgroup':
+    #     path   => "HKLM\\SYSTEM\\CurrentControlSet\\Control\\Power\\User\\PowerSchemes\\${power_scheme_guid}\\${
+    #         sleep_subgroup_guid}",
+    #     ensure => present
+    # }
+
+    registry_key { 'DisplaySleepSetting':
+        path   => "HKLM\\SYSTEM\\CurrentControlSet\\Control\\Power\\User\\PowerSchemes\\${power_scheme_guid}\\${
+            sleep_subgroup_guid}\\${sleep_after_guid}",
+        ensure => present
+    }
+
+    registry_value { 'DisplaySleep':
+        path => "HKLM\\SYSTEM\\CurrentControlSet\\Control\\Power\\User\\PowerSchemes\\${power_scheme_guid}\\${
+            sleep_subgroup_guid}\\${sleep_after_guid}\ACSettingIndex",
+        type => 'dword',
+        data => 1800
+    }
+
+    # When dealing with Registry: HIVE\Key:value=data
+    Reg_acl['CustomSettings'] -> Registry_key['DisplaySleepSetting'] -> Registry_value['DisplaySleep']
 }
