84a55b771f35bdf18626daa1a5e1bbf770a5984d
Grant Cohoe <grant@grantcohoe.com>
2019-05-05 16:57:40 -0400

    fixes

--- manifests/profile/lancache/web.pp
+++ manifests/profile/lancache/web.pp
@@ -3,6 +3,24 @@
 #
 class profile::lancache::web {
 
+    # These dirs are not managed by the nginx module
+    $proxy_cache_directories = [
+        '/mnt/LanCache/cache/',
+        '/mnt/LanCache/cache/installs',
+        '/mnt/LanCache/cache/other'
+    ]
+    file { $proxy_cache_directories:
+        ensure => directory,
+        owner  => $::nginx::params::daemon_user,
+        group  => $::nginx::params::daemon_group,
+    }
+
+    file { '/var/log/lancache':
+        ensure => directory,
+        owner  => $::nginx::params::daemon_user,
+        group  => $::nginx::params::daemon_group,
+    }
+
     # Log formatting comes from https://github.com/multiplay/lancache/blob/master/lancache/logging
     class { '::nginx':
         proxy_temp_path => '/mnt/LanCache/cache/tmp',
@@ -10,20 +28,8 @@ class profile::lancache::web {
         log_format      => {
             'main' =>
             '$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent" "$upstream_cache_status" "$host" "$http_range" "$request_time" "$upstream_response_time"'
-        }
-    }
-
-    $proxy_cache_directories = [
-        '/mnt/LanCache/cache/',
-        '/mnt/LanCache/cache/tmp',
-        '/mnt/LanCache/cache/installs',
-        '/mnt/LanCache/cache/other'
-    ]
-    file { $proxy_cache_directories:
-        ensure  => directory,
-        owner   => $::nginx::params::daemon_user,
-        group   => $::nginx::params::daemon_group,
-        require => Class['::nginx']
+        },
+        require         => [ File['/var/log/lancache'], File[$proxy_cache_directories] ]
     }
 
     $proxy_cache_paths = {
@@ -53,14 +59,6 @@ class profile::lancache::web {
         content => template('cfcc/nginx/proxy-cache.conf.erb'),
         notify  => Service['nginx']
     }
-
-    file { '/var/log/lancache':
-        ensure => directory,
-        owner  => $::nginx::params::daemon_user,
-        group  => $::nginx::params::daemon_group,
-        require => Class['::nginx']
-    }
-
 
 
 }
