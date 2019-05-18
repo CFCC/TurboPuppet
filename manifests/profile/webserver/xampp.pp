6f8e451c48c1ea9411bde59f5d2efdae41637153
Grant Cohoe <grant@grantcohoe.com>
2019-05-18 18:28:34 -0400

    drop rules

--- manifests/profile/webserver/xampp.pp
+++ manifests/profile/webserver/xampp.pp
@@ -8,19 +8,4 @@ class profile::webserver::xampp {
     }
 
     package { $package_name: }
-
-    windows_firewall::exception { 'Allow XAMPP HTTP':
-        ensure       => present,
-        direction    => 'in',
-        action       => 'Allow',
-        enabled      => 'yes',
-        protocol     => 'TCP',
-        local_port   => '80',
-        remote_port  => 'any',
-        # display_name => 'File and Printer Sharing (Echo Request - ICMPv4-In)',
-        display_name => 'TurboPuppet XAMPP HTTP Allow',
-        # description  => 'Echo Request messages are sent as ping requests to other nodes.',
-        description  => 'Allow httpd',
-        program      => 'C:\xampp\apache\bin\httpd.exe'
-    }
 }
