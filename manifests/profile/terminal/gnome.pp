b4713f69192c3b75728acc1708dc2c927023145d
Grant Cohoe <grant@grantcohoe.com>
2019-06-14 09:59:21 -0400

    gnome-terminal

--- manifests/profile/terminal/gnome.pp
+++ manifests/profile/terminal/gnome.pp
@@ -11,6 +11,43 @@ class profile::terminal::gnome {
                 mode   => '0755',
                 owner  => $turbosite::camper_username
             }
+
+            Dconf::Setting {
+                user => $::turbosite::camper_username,
+                uid  => $::turbosite::camper_uid
+            }
+
+            dconf::setting { 'SetClockFormat':
+                key   => '/org/gnome/terminal/legacy/default-show-menubar',
+                value => false,
+            }
+
+            $profile_guid = 'b1dcc9dd-5262-4d8d-a863-c897e6d979b9'
+            $terminal_profile_settings = {
+                'visible-name'                    => 'camper',
+                'use-theme-colors'                => false,
+                'foreground-color'                => 'rgb(255,255,255)',
+                'background-color'                => 'rgb(0,0,0)',
+                'audible-bell'                    => false,
+                'use-transparent-background'      => true,
+                'background-transparency-percent' => 20,
+                'login-shell'                     => 'true'
+            }
+
+            $terminal_profile_settings.each |String $key, String $value| {
+                dconf::setting { $key:
+                    key   => "/org/gnome/terminal/legacy/profiles:/:${profile_guid}/$key",
+                    value => $value
+                }
+            } ->
+            dconf::setting { 'default-profile-list':
+                key   => '/org/gnome/terminal/legacy/profiles:/list',
+                value => "['${profile_guid}']"
+            } ->
+            dconf::setting { 'default-profile':
+                key   => '/org/gnome/terminal/legacy/profiles:/default',
+                value => $profile_guid
+            }
         }
         default: {}
     }
