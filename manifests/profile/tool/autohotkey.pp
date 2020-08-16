46d6401f41a2c9cef23e72dec38dd201aa705eb5
Grant Cohoe <grant@grantcohoe.com>
2020-08-16 12:31:05 -0400

    commentary

--- manifests/profile/tool/autohotkey.pp
+++ manifests/profile/tool/autohotkey.pp
@@ -4,6 +4,7 @@
 class profile::tool::autohotkey {
     package { 'autohotkey': }
 
+    # https://autohotkey.com/board/topic/119664-reloading-script-from-command-prompt/
     exec { 'ReloadAutoHotkey':
       command     => 'AutoHotkey.exe /r',
       refreshonly => true,
