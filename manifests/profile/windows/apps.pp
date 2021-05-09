fb08d0dc17a45afb75a6fa92674ef18b9818a748
Grant Cohoe <grant@grantcohoe.com>
2021-05-09 17:01:16 -0400

    more candy to crush

--- manifests/profile/windows/apps.pp
+++ manifests/profile/windows/apps.pp
@@ -2,7 +2,11 @@
 # Various built-in Windows apps. This is probably more than Candy Crush?
 #
 class profile::windows::apps {
-  appxpackage { 'king.com.CandyCrushSaga':
+  $trash_apps = [
+    'king.com.CandyCrushSaga',
+    'king.com.CandyCrushFriends',
+  ]
+  appxpackage { $trash_apps:
     ensure => 'absent'
   }
 }
