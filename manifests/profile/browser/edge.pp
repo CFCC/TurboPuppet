eb8e498b2121bc6ff03ce2d1abd5011d0f877f8d
Grant Cohoe <grant@grantcohoe.com>
2021-05-11 21:53:25 -0400

    fixes #12, removes edge in a semi-deterministic manor

--- manifests/profile/browser/edge.pp
+++ manifests/profile/browser/edge.pp
@@ -4,16 +4,13 @@
 # https://lifehacker.com/how-to-uninstall-edge-chromium-when-windows-10-wont-let-1844297854
 #
 class profile::browser::edge {
-  # This does not work. Windows says:
-  #   C:\WINDOWS\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe failed. This app is part of Windows and cannot be
-  #   uninstalled on a per-user basis. An administrator can attempt to remove the app from the computer using Turn Windows
-  #   Features on or off. However, it may not be possible to uninstall the app.
-  # It is more unfortunate.
-  # appxpackage { 'Microsoft.MicrosoftEdge':
-  #   ensure => 'absent',
-  # }
+  exec { 'UninstallEdge':
+    command => '& "$(Get-ChildItem -Recurse -Filter setup.exe | select -ExpandProperty Directory | % { $_.FullName })\Setup.exe" -uninstall -system-level -verbose-logging -force-uninstall',
+    onlyif  => psexpr('Get-Item -Path "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"')
+  }
 
-  # However, running the installer method actually does work:
-  # C:\Program Files (x86)\Microsoft\Edge\Application\90.0.818.56\Installer\setup.exe -uninstall -system-level -verbose-logging -force-uninstall
-  # @TODO #12 need a way to make this not version-specific and idempotent.
+  file { 'EdgeDesktopShortcut':
+    ensure => 'absent',
+    path => "C:/Users/${turbosite::camper_username}/Desktop/Microsoft Edge.lnk"
+  }
 }
