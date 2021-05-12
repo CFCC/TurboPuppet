23111041402f5d74187b62c2948cbf17cc01a585
Grant Cohoe <grant@grantcohoe.com>
2021-05-11 22:10:21 -0400

    #12 documentation and path fix

--- manifests/profile/browser/edge.pp
+++ manifests/profile/browser/edge.pp
@@ -4,8 +4,12 @@
 # https://lifehacker.com/how-to-uninstall-edge-chromium-when-windows-10-wont-let-1844297854
 #
 class profile::browser::edge {
+  # This is some hacks. Basically use PowerShell to find the setup.exe since it lives in a per-version
+  # directory then run it with the required flags. Only do this if the Edge application exe is present.
+  # https://stackoverflow.com/questions/13126175/get-full-path-of-the-files-in-powershell
+  # https://stackoverflow.com/questions/4639894/executing-an-exe-file-using-a-powershell-script
   exec { 'UninstallEdge':
-    command => '& "$(Get-ChildItem -Recurse -Filter setup.exe | select -ExpandProperty Directory | % { $_.FullName })\Setup.exe" -uninstall -system-level -verbose-logging -force-uninstall',
+    command => '& "$(Get-ChildItem -Path \"C:\Program Files (x86)\Microsoft\Edge\Application\" -Recurse -Filter setup.exe | select -ExpandProperty Directory | % { $_.FullName })\Setup.exe" -uninstall -system-level -verbose-logging -force-uninstall',
     onlyif  => psexpr('Get-Item -Path "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"')
   }
 
