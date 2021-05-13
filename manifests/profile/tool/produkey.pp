93e28e6c6632d3d28003feb98b469336b3c4f665
Grant Cohoe <grant@grantcohoe.com>
2021-05-13 19:37:27 -0400

    closes #24, disables produkey until something changes

--- manifests/profile/tool/produkey.pp
+++ manifests/profile/tool/produkey.pp
@@ -2,5 +2,7 @@
 # ProduKey Windows Product Key Reporter
 #
 class profile::tool::produkey {
-  package { 'produkey.install': }
+  # #24 this is being falsely reported as a virus by Microsoft. Workarounds
+  # are not fun so I'm just going to disable it until we find it is needed.
+  # package { 'produkey.install': }
 }
