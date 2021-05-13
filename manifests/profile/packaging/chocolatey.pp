26b9b2a7255dd9a921d47611ffdbb5b856569d35
Grant Cohoe <grant@grantcohoe.com>
2021-05-12 23:54:22 -0400

    fix chocolatey first run issues

--- manifests/profile/packaging/chocolatey.pp
+++ manifests/profile/packaging/chocolatey.pp
@@ -5,4 +5,14 @@
 #
 class profile::packaging::chocolatey {
   class { 'chocolatey': }
+
+  # Sometime since the 2019 camp season Choco started needing this in order
+  # to not explode on the first Puppet run. Subsequent runs were fine but the
+  # first exploded in a red fireball of death logs. While this likely has
+  # some security implications it seems to get the job done.
+  exec { 'DisableConfirmation':
+    command => 'C:\ProgramData\chocolatey\choco.exe feature enable -n allowGlobalConfirmation',
+    onlyif  => psexpr("(C:\\ProgramData\\chocolatey\\choco.exe feature list | Select-String -Pattern 'allowGlobalConfirmation') -Match \"[ ]\""),
+    require => Class['chocolatey']
+  }
 }
