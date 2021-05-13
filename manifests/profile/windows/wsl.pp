a25b7d6b88e7838cd679e0d157fd955379e61a27
Grant Cohoe <grant@grantcohoe.com>
2021-05-12 22:51:33 -0400

    disable wsl for now

--- manifests/profile/windows/wsl.pp
+++ manifests/profile/windows/wsl.pp
@@ -2,8 +2,10 @@
 #
 #
 class profile::windows::wsl {
-  package { 'wsl2': } ->
-  package { 'wsl-ubuntu-2004': }
+  # package { 'wsl2': }
+  # wsl-ubuntu-2004 needs package wsl2 to be installed first and either a shell
+  # refresh or reboot before the first can be installed. Figure out a way to do that.
+  # package { 'wsl-ubuntu-2004': }
 
   # @TODO
   # * %PROGRAMDATA%\Choco\lib\tools\wsl-ubuntu\ubuntu1804.exe --default-user root|camper
