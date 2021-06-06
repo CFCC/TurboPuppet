de4638bf345c4ae30c606131cdb52626a091c9a9
Grant Cohoe <grant@grantcohoe.com>
2021-06-06 18:50:20 -0400

    derp

--- manifests/profile/game/origin.pp
+++ manifests/profile/game/origin.pp
@@ -2,7 +2,7 @@
 # EA Origin
 # This will yell about LAN caching but it's up to the user to accept.
 #
-class profile::game::epic {
+class profile::game::origin {
   package { 'origin':
     notify => Exec['CleanupDesktopShortcuts']
   }
