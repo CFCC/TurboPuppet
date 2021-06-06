dd31db0c5e8a7defda921eb7b7476eec697d9c60
Grant Cohoe <grant@grantcohoe.com>
2021-06-06 18:42:52 -0400

    closes #37, removes server junk

--- manifests/role/server/lancache.pp
+++
@@ -1,9 +0,0 @@
-#
-#
-#
-# @TODO inherit role::server
-class role::server::lancache inherits role::server {
-  include profile::lancache::web::nginx
-  include profile::lancache::web::sniproxy
-  include profile::lancache::dns
-}
