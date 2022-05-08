5393fbbe8db923858d7e1cf669acd3b4d02181ab
Grant Cohoe <grant@grantcohoe.com>
2022-05-08 14:22:25 -0400

    fixes #42 make certificates in the right place and for windows only

--- manifests/profile/tls/certificates.pp
+++ manifests/profile/tls/certificates.pp
@@ -3,26 +3,40 @@
 #
 class profile::tls::certificates {
 
-  $certificate_files = [
-    'tars-ca-v1.crt',
-    'tars-ca-v2.crt',
-  ]
-
   $local_cert_directory = $::kernel ? {
     'windows' => 'C:/CampFitch/usr/share/certificates',
     default   => '/etc/pki/ca-trust/source/anchors'
   }
 
-  $certificate_files.each |String $cert| {
-    file { $cert:
-      ensure => present,
-      path   => "${local_cert_directory}/${cert}",
-      source => "puppet:///modules/cfcc/certificates/${cert}"
-    }
+  file { $local_cert_directory:
+    ensure  => 'directory',
+    source  => 'puppet:///modules/cfcc/certificates',
+    recurse => 'remote',
+    purge   => false,
+    replace => false,
   }
-  case $::operatingsystem {
+
+  case $::kernel {
     'windows': {
+      # CA = Current User or Local Computer \ Intermediate Certification Authorities \ Certificates
+      # Root = Current User or Local Computer \ Trusted Root Certification Authorities \ Certificates
+      Sslcertificate {
+        location   => $local_cert_directory,
+        root_store => 'LocalMachine',
+        store_dir  => 'Root',
+      }
+
+      sslcertificate { "tars-ca-v1.crt":
+        thumbprint => 'B4AB05D2F7C362411E9118EA08DE991FEA3A391E'
+      }
+
+      sslcertificate { "tars-ca-v2.crt":
+        thumbprint => '1F30886A00CEA39B8D167A244F2712C5050ED429'
+      }
     }
-    default: {}
+    'Linux': {
+      # In theory this should just contain an exec{updatecatrust} and a conditional
+      # notify on the directory. Life is easy.
+    }
   }
 }
