96ee56be57cb198c3c342356b9c46a0f35b834d8
Grant Cohoe <grant@grantcohoe.com>
2022-05-07 21:23:56 -0400

    disable news and feeds

--- manifests/profile/desktop/explorer.pp
+++ manifests/profile/desktop/explorer.pp
@@ -232,6 +232,26 @@ class profile::desktop::explorer {
     require => Registry_key['WindowsSearch'],
   }
 
+  # Disable News and Interests
+  # https://www.prajwaldesai.com/disable-news-and-interests-in-windows-10/
+  # I'm not sure this actually works
+  hkcu { 'ShellFeedsTaskbarViewMode':
+    key   => 'SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds',
+    value => 'ShellFeedsTaskbarViewMode',
+    data  => 2
+  }
+
+  # https://winaero.com/add-or-remove-news-and-interests-button-from-taskbar-in-windows-10/
+  registry_key { 'WindowsFeeds':
+    path   => 'HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds',
+    ensure => present,
+  } ->
+  registry_value { 'EnableFeeds':
+    path => 'HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds\EnableFeeds',
+    type => 'dword',
+    data => 0
+  }
+
   exec { 'Reload Explorer':
     command     => "Stop-Process -ProcessName explorer",
     refreshonly => true,
