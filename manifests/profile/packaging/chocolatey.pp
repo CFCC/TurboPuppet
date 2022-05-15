e146d3ad18ae233cc9ef805da611a89b9b5b6cfa
Grant Cohoe <grant@grantcohoe.com>
2022-05-15 12:37:22 -0400

    commentary

--- manifests/profile/packaging/chocolatey.pp
+++ manifests/profile/packaging/chocolatey.pp
@@ -16,6 +16,11 @@ class profile::packaging::chocolatey {
     require => Class['chocolatey']
   }
 
+  # Had a bright idea to use a shared network mount as the Chocolatey package cache location.
+  # Worked alright except that it wouldn't come up on reboot and other weird install errors
+  # manifested themselves. Like packages refused to install. It caches about 1.6GB of content
+  # so that's not nothing but it's worthless if it isn't reliable. Disabling until some
+  # other day.
   # This is used in tandem with the mount defined in profile::storage::mountpoints.
   chocolateyconfig { 'cachelocation':
     #value   => "F:\\windows\\Chocolatey",
