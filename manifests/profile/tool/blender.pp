75cc8d3c797ff85686e77395aa85b436561d685c
Grant Cohoe <grant@grantcohoe.com>
2024-05-24 14:17:35 -0400

    blender clean

--- manifests/profile/tool/blender.pp
+++ manifests/profile/tool/blender.pp
@@ -2,11 +2,15 @@
 # Blender
 #
 class profile::tool::blender {
-  package { 'blender': }
+  package { 'blender': 
+    notify => $::kernel ? {
+      'windows' => Notify['CleanupBlenderShortcut'],
+      default   => undef,
+    }
+  }
 
-  file { 'BlenderDesktopShortcut':
-    ensure => 'absent',
-    path   => "C:/Users/${turbosite::camper_username}/Desktop/Blender 3.5.lnk",
-    require => Package['blender']
+  exec { 'CleanupBlenderShortcut':
+    command     => "Remove-Item -Path 'C:\\Users\\${turbosite::camper_username}\\Desktop\\Blender\*.lnk'"
+    refreshonly => true
   }
 }
