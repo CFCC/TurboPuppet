7cf2d722edd3bd297bbc7f709b7045a9c9e468ba
Grant Cohoe <grant@grantcohoe.com>
2024-05-24 14:26:51 -0400

    wireshark

--- manifests/profile/tool/wireshark.pp
+++ manifests/profile/tool/wireshark.pp
@@ -9,14 +9,15 @@ class profile::tool::wireshark {
   }
 
   # Some platforms require extra things to make it work
-  case $::operatingsystem {
-    'windows': {
-      # Note - autohotkey doesn't behave well in VMs. Works fine for hardware.
-      package { 'winpcap':
-        before => [ Package[$package_name] ]
-      }
-    }
-  }
+  # case $::operatingsystem {
+  #   'windows': {
+  #     # Note - autohotkey doesn't behave well in VMs. Works fine for hardware.
+  #     # winpcap is long abandoned. npcap doesnt have choco. Haven't found an automated solution yet.
+  #     # package { 'winpcap':
+  #     #   before => [ Package[$package_name] ]
+  #     # }
+  #   }
+  # }
 
   package { $package_name: }
 }
