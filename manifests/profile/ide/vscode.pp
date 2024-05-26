04c7e9e0c6ed9e35be128005f75244eeed75ed35
Grant Cohoe <grant@grantcohoe.com>
2024-05-26 00:08:01 -0400

    vscode notify

--- manifests/profile/ide/vscode.pp
+++ manifests/profile/ide/vscode.pp
@@ -1,11 +1,12 @@
 #
-#
+# Visual Studio Code
 #
 class profile::ide::vscode {
   package { 'vscode':
     install_options => [
       '--params "/NoDesktopIcon /NoQuicklaunchIcon"'
-    ]
+    ],
+    notify => Exec['CleanupDesktopShortcuts'],
   }
 
   package { ['vscode-python', 'vscode-java']: 
