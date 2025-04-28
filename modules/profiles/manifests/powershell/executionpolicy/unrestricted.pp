8ecf65b43aeb6a3960c79cca11e1c19e2a78c85e
Grant <grant@grantcohoe.com>
2025-04-27 23:00:44 -0400

    powershell

--- modules/profiles/manifests/powershell/executionpolicy/unrestricted.pp
+++ modules/profiles/manifests/powershell/executionpolicy/unrestricted.pp
@@ -1,12 +1,12 @@
 #
 #
 #
-class profile::powershell::executionpolicy::unrestricted {
+class profiles::powershell::executionpolicy::unrestricted {
   $scope = 'LocalMachine'
   $policy = 'Unrestricted'
 
   exec { 'SetExecutionPolicy':
     command => "Set-ExecutionPolicy -Scope ${scope} -ExecutionPolicy ${policy}",
-    onlyif  => psexpr("(Get-ExecutionPolicy -Scope ${scope}) -ne '${policy}'"),
+    onlyif  => cfcc::psexpr("(Get-ExecutionPolicy -Scope ${scope}) -ne '${policy}'"),
   }
 }
