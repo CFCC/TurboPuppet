571b0058bd83a4ea841020561ddc0cd9c0342872
Grant <grant@grantcohoe.com>
2025-05-01 11:09:38 -0400

    add default

--- modules/profiles/manifests/puppet/turbopuppet.pp
+++ modules/profiles/manifests/puppet/turbopuppet.pp
@@ -2,15 +2,21 @@
 # Manage TurboPuppet scripts
 #
 class profiles::puppet::turbopuppet {
+  $environment = $facts['agent_specified_environment'] ? {
+    undef   => 'production',
+    ''      => 'production',
+    default => $facts['agent_specified_environment'],
+  }
+
   file { 'TurboPuppet.ps1':
     ensure => file,
     path   => 'C:\CampFitch\bin\TurboPuppet.ps1',
-    source => "C:\\ProgramData\\PuppetLabs\\code\\environments\\${facts['agent_specified_environment']}\\scripts\\TurboPuppet.ps1",
+    source => "C:\\ProgramData\\PuppetLabs\\code\\environments\\${environment}\\scripts\\TurboPuppet.ps1",
   }
 
   file { 'Install-TurboPuppet.ps1':
     ensure => file,
     path   => 'C:\CampFitch\bin\Install-TurboPuppet.ps1',
-    source => "C:\\ProgramData\\PuppetLabs\\code\\environments\\${facts['agent_specified_environment']}\\scripts\\Install-TurboPuppet.ps1",
+    source => "C:\\ProgramData\\PuppetLabs\\code\\environments\\${environment}\\scripts\\Install-TurboPuppet.ps1",
   }
 }
