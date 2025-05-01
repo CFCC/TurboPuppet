091d6e595d24a6f3b4c71e2bf98f8b83fa3a0bc5
Grant <grant@grantcohoe.com>
2025-04-30 21:56:24 -0400

    gimp needs to clean up

--- modules/profiles/manifests/editor/gimp.pp
+++ modules/profiles/manifests/editor/gimp.pp
@@ -2,5 +2,11 @@
 # GNU Image Manipulation Program
 #
 class profiles::editor::gimp {
-  package { 'gimp': }
+  $package_ensure = $facts['os']['family'] ? {
+    'windows' => Exec['CleanupDesktopShortcuts'],
+    default   => undef,
+  }
+  package { 'gimp':
+    ensure => $package_ensure,
+  }
 }
