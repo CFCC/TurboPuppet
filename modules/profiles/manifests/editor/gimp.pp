50a5f7f8f6d382989b60477cae2abb239f3f0e7c
Grant <grant@grantcohoe.com>
2025-04-30 22:05:25 -0400

    change exec

--- modules/profiles/manifests/editor/gimp.pp
+++ modules/profiles/manifests/editor/gimp.pp
@@ -2,11 +2,11 @@
 # GNU Image Manipulation Program
 #
 class profiles::editor::gimp {
-  $package_notify = $facts['os']['family'] ? {
-    'windows' => Exec['CleanupDesktopShortcuts'],
-    default   => undef,
-  }
-  package { 'gimp':
-    notify => $package_notify,
+  package { 'gimp': }
+
+  exec { 'CleanupGimpDesktopShortcut':
+    command     => "Remove-Item -Path 'C:\\Users\\${lookup('camper_username')}\\Desktop\\GIMP 3.0.2-1.lnk'",
+    refreshonly => true,
+    subscribe   => Package['gimp'],
   }
 }
