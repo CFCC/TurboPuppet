2322fbf3bd05f4aac7cecfbf99a19c6fe9f5eff1
Grant <grant@grantcohoe.com>
2025-05-11 12:43:56 -0400

    deal with return bullshit

--- modules/cfcc/manifests/driver.pp
+++ modules/cfcc/manifests/driver.pp
@@ -7,6 +7,12 @@ define cfcc::driver (
       exec { "Install-${name}":
         command => "pnputil.exe /add-driver ${path} /install",
         unless  => cfcc::psexpr("pnputil.exe /enum-drivers | findstr /i \"${name}\""),
+        # https://learn.microsoft.com/en-us/windows-hardware/drivers/devtest/pnputil-return-values
+        returns => [
+          0, # Success
+          # 259, # Nothing on this system has the driver
+          3010, # Success, restart required
+        ],
       }
     }
     'absent': {
