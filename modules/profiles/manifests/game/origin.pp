882ae773dfaee5a2117ebc6206c8d214cdbcdc27
Grant <grant@grantcohoe.com>
2025-05-26 13:01:31 -0400

    origin is now ea app

--- modules/profiles/manifests/game/origin.pp
+++ modules/profiles/manifests/game/origin.pp
@@ -3,7 +3,8 @@
 # This will yell about LAN caching but it's up to the user to accept.
 #
 class profiles::game::origin {
-  package { 'origin':
+  # 2025 they replaced Origin with EA App.
+  package { 'ea-app':
     notify => Exec['CleanupDesktopShortcuts'],
   }
 }
