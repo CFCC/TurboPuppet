97181e11a6c4b5f459cb948c5b0db653e5889fa5
Grant <grant@grantcohoe.com>
2025-05-28 22:12:29 -0400

    driver

--- modules/profiles/manifests/driver/hp.pp
+++ modules/profiles/manifests/driver/hp.pp
@@ -4,6 +4,6 @@
 class profiles::driver::hp {
   # HP01 is a special snowflake that has a Radeon GPU but with an Intel CPU.
   unless $facts['networking']['hostname'] =~ /(?i:cfcchp01)/ {
-    include profile::driver::cpu::ryzen
+    include profiles::driver::cpu::ryzen
   }
 }
