a1bac224dd104a4b9775054c800f574c8a4cda48
Grant Cohoe <grant@grantcohoe.com>
2026-05-13 23:15:19 -0400

    disable ea app on install

--- modules/profiles/manifests/game/origin.pp
+++ modules/profiles/manifests/game/origin.pp
@@ -3,9 +3,35 @@
 # This will yell about LAN caching but it's up to the user to accept.
 #
 class profiles::game::origin {
+  $package_notify = $facts['os']['family'] ? {
+    'windows' => [Exec['CleanupDesktopShortcuts'], Exec['kill ea desktop app']],
+    default   => Exec['CleanupDesktopShortcuts'],
+  }
+
+  $install_options = $facts['os']['family'] ? {
+    'windows' => '--ignore-checksums',
+    default   => undef,
+  }
+
   # 2025 they replaced Origin with EA App.
   package { 'ea-app':
-    notify          => Exec['CleanupDesktopShortcuts'],
-    install_options => '--ignore-checksums',
+    notify          => $package_notify,
+    install_options => $install_options,
+  }
+
+  case $facts['os']['family'] {
+    'windows': {
+      cfcc::hkcu { 'DisableEADesktopAutostart':
+        ensure => absent,
+        key    => 'Software\Microsoft\Windows\CurrentVersion\Run',
+        value  => 'EADM',
+      }
+
+      exec { 'kill ea desktop app':
+        command     => 'Sleep 15; Stop-Process -ProcessName EADesktop',
+        refreshonly => true,
+      }
+    }
+    default: {}
   }
 }
