66eb9a3c1a37a05f1c4fd17c1c0e9a4e878d6f00
Grant Cohoe <grant@grantcohoe.com>
2026-05-16 21:49:40 -0400

    switch to package for gmktecs

--- modules/profiles/manifests/driver/gmktec.pp
+++ modules/profiles/manifests/driver/gmktec.pp
@@ -7,15 +7,15 @@ class profiles::driver::gmktec (
   String $driver_root = $profiles::driver::base::driver_root,
 ) inherits profiles::driver::base {
   # As of 2025-05 the amd-ryzen-chipset package does not support the AI MAX 395.
-  # include profiles::driver::cpu::ryzen
+  include profiles::driver::cpu::ryzen
 
   # @TODO this is kinda janky. AMD will likely update a new package soon.
   # But need a better way to instrument this kind of driver install anyway.
-  exec { 'Driver Bundle Installation':
-    command     => "${driver_root}\\GMKtec\\AllDriverInstall.cmd",
-    refreshonly => true,
-    require     => File['Camp Drivers'],
-    subscribe   => File['Camp Drivers'],
-    returns     => [0, 1],
-  }
+  # exec { 'Driver Bundle Installation':
+  #   command     => "${driver_root}\\GMKtec\\AllDriverInstall.cmd",
+  #   refreshonly => true,
+  #   require     => File['Camp Drivers'],
+  #   subscribe   => File['Camp Drivers'],
+  #   returns     => [0, 1],
+  # }
 }
