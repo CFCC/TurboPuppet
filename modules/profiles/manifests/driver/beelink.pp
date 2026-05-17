5226de17a44af8c2d2aee7f57f80b3d9a11cbf80
Grant Cohoe <grant@grantcohoe.com>
2026-05-16 21:11:21 -0400

    begin overhaul of drivers

--- modules/profiles/manifests/driver/beelink.pp
+++ modules/profiles/manifests/driver/beelink.pp
@@ -1,36 +1,36 @@
 #
 #
 #
-class profiles::driver::beelink {
+class profiles::driver::beelink inherits profiles::driver::base {
   include profiles::driver::cpu::ryzen
 
-  $driver_root = "C:\\CampFitch\\opt\\Drivers"
+  # $driver_root = "C:\\CampFitch\\opt\\Drivers"
 
-  file { 'Drivers':
-    ensure  => 'directory',
-    source  => "${lookup('campfs_uri')}\\Drivers",
-    path    => $driver_root,
-    recurse => 'remote',
-    purge   => false,
-    replace => false,
-  }
+  # file { 'Drivers':
+  #   ensure  => 'directory',
+  #   source  => "${lookup('campfs_uri')}\\Drivers",
+  #   path    => $driver_root,
+  #   recurse => 'remote',
+  #   purge   => false,
+  #   replace => false,
+  # }
 
-  Cfcc::Driver {
-    ensure  => present,
-    require => File['Drivers'],
-  }
+  # Cfcc::Driver {
+  #   ensure  => present,
+  #   require => File['Drivers'],
+  # }
 
-  cfcc::driver { 'amdacpbt.inf':
-    path => "${driver_root}\\Beelink\\ACPBtAfd\\WT64A\\amdacpbt.inf",
-  }
+  # cfcc::driver { 'amdacpbt.inf':
+  #   path => "${driver_root}\\Beelink\\ACPBtAfd\\WT64A\\amdacpbt.inf",
+  # }
 
-  if $facts['networking']['hostname'] =~ /(?i:cfccbeelink05)/ {
-    cfcc::driver { 'amdacpbus2.inf':
-      path => "${driver_root}\\Beelink\\ACPBus2\\WT64A\\amdacpbus2.inf",
-    }
-  } else {
-    cfcc::driver { 'amdacpbus.inf':
-      path => "${driver_root}\\Beelink\\ACPBus\\WT64A\\amdacpbus.inf",
-    }
-  }
+  # if $facts['networking']['hostname'] =~ /(?i:cfccbeelink05)/ {
+  #   cfcc::driver { 'amdacpbus2.inf':
+  #     path => "${driver_root}\\Beelink\\ACPBus2\\WT64A\\amdacpbus2.inf",
+  #   }
+  # } else {
+  #   cfcc::driver { 'amdacpbus.inf':
+  #     path => "${driver_root}\\Beelink\\ACPBus\\WT64A\\amdacpbus.inf",
+  #   }
+  # }
 }
