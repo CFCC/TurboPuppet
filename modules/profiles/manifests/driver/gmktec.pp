a6d681555dc003a7de9c39f18aef74f7d8e582aa
Grant Cohoe <grant@grantcohoe.com>
2026-05-16 23:02:19 -0400

    try cwd

--- modules/profiles/manifests/driver/gmktec.pp
+++ modules/profiles/manifests/driver/gmktec.pp
@@ -10,7 +10,12 @@ class profiles::driver::gmktec {
     command     => "${platform_source}\\AllDriverInstall.cmd",
     # refreshonly => true,
     returns     => [0, 1],
+    cwd         => $platform_source,
   }
+
+  # 05_AMD_BT_1.1042.0.527\source\BT\mtkbtfilter.inf
+  # 04_AMD_WiFi_5.5.0.3760\source\mtkwecx.inf
+
 
   # As of 2025-05 the amd-ryzen-chipset package does not support the AI MAX 395.
   # This is still true as of 2026-05.
