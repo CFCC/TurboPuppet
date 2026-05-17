0b70d4c658709d5aae13db3ce32443ba084bd600
Grant Cohoe <grant@grantcohoe.com>
2026-05-16 22:49:19 -0400

    gmktec crap

--- modules/profiles/manifests/driver/gmktec.pp
+++ modules/profiles/manifests/driver/gmktec.pp
@@ -4,6 +4,15 @@
 class profiles::driver::gmktec {
   $platform_source = "${lookup('campfs_uri')}\\Drivers\\GMKtec"
 
+  # @TODO this is kinda janky. AMD will likely update a new package soon.
+  # But need a better way to instrument this kind of driver install anyway.
+  exec { 'Driver Bundle Installation':
+    command     => "${platform_source}\\AllDriverInstall.cmd",
+    refreshonly => true,
+    returns     => [0, 1],
+  }
+
   # As of 2025-05 the amd-ryzen-chipset package does not support the AI MAX 395.
-  include profiles::driver::cpu::ryzen
+  # This is still true as of 2026-05.
+  # include profiles::driver::cpu::ryzen
 }
