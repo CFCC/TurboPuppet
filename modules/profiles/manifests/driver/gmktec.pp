98072079972217ecfa1bbae2ca07248f8ddacbca
Grant Cohoe <grant@grantcohoe.com>
2026-05-16 23:11:09 -0400

    commentary

--- modules/profiles/manifests/driver/gmktec.pp
+++ modules/profiles/manifests/driver/gmktec.pp
@@ -1,5 +1,5 @@
 #
-# Install the GMKtec driver bundle.
+# Drivers for GMKtec devices.
 #
 class profiles::driver::gmktec {
   $platform_source = "${lookup('campfs_uri')}\\Drivers\\GMKtec"
@@ -14,8 +14,6 @@ class profiles::driver::gmktec {
     replace => false,
   }
 
-  # 05_AMD_BT_1.1042.0.527\source\BT\mtkbtfilter.inf
-  # 04_AMD_WiFi_5.5.0.3760\source\mtkwecx.inf
   exec { 'Driver Bundle Installation':
     command => "${local_root}\\AllDriverInstall.cmd",
     unless  => cfcc::psexpr("pnputil.exe /enum-drivers | findstr /i \"mtkbtfilter.inf\""),
@@ -23,7 +21,11 @@ class profiles::driver::gmktec {
     returns => [0, 1],
   }
 
+  # Known drivers to install from the bundle.
+  # 05_AMD_BT_1.1042.0.527\source\BT\mtkbtfilter.inf
+  # 04_AMD_WiFi_5.5.0.3760\source\mtkwecx.inf
+
   # As of 2025-05 the amd-ryzen-chipset package does not support the AI MAX 395.
-  # This is still true as of 2026-05.
+  # As of 2026-05 this is still true.
   # include profiles::driver::cpu::ryzen
 }
