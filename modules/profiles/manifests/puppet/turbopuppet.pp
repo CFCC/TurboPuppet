278598af5227d995cacdf19ad9d15e3c6e5f9815
Grant Cohoe <grant@grantcohoe.com>
2026-05-17 20:25:23 -0400

    turbo

--- modules/profiles/manifests/puppet/turbopuppet.pp
+++ modules/profiles/manifests/puppet/turbopuppet.pp
@@ -8,15 +8,35 @@ class profiles::puppet::turbopuppet {
     default => $facts['agent_specified_environment'],
   }
 
-  file { 'TurboPuppet.ps1':
-    ensure => file,
-    path   => 'C:\CampFitch\bin\TurboPuppet.ps1',
-    source => "C:\\ProgramData\\PuppetLabs\\code\\environments\\${environment}\\scripts\\TurboPuppet.ps1",
-  }
+  case $facts['kernel'] {
+    'windows': {
+      file { 'TurboPuppet.ps1':
+        ensure => file,
+        path   => 'C:/CampFitch/bin/TurboPuppet.ps1',
+        source => "C:/ProgramData/PuppetLabs/code/environments/${environment}/scripts/TurboPuppet.ps1",
+      }
 
-  file { 'Install-TurboPuppet.ps1':
-    ensure => file,
-    path   => 'C:\CampFitch\bin\Install-TurboPuppet.ps1',
-    source => "C:\\ProgramData\\PuppetLabs\\code\\environments\\${environment}\\scripts\\Install-TurboPuppet.ps1",
+      file { 'Install-TurboPuppet.ps1':
+        ensure => file,
+        path   => 'C:/CampFitch/bin/Install-TurboPuppet.ps1',
+        source => "C:/ProgramData/PuppetLabs/code/environments/${environment}/scripts/Install-TurboPuppet.ps1",
+      }
+    }
+    'Darwin', 'Linux': {
+      file { 'turbopuppet.sh':
+        ensure => file,
+        path   => '/opt/CampFitch/bin/turbopuppet.sh',
+        source => "/etc/puppetlabs/code/environments/${environment}/scripts/turbopuppet.sh",
+        mode   => '0755',
+      }
+
+      file { 'install-turbopuppet.sh':
+        ensure => file,
+        path   => '/opt/CampFitch/bin/install-turbopuppet.sh',
+        source => "/etc/puppetlabs/code/environments/${environment}/scripts/install-turbopuppet.sh",
+        mode   => '0755',
+      }
+    }
+    default: {}
   }
 }
