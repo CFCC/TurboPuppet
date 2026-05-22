6ac7da5930258889a68eca8351abf08b1adb9bb9
Grant Cohoe <grant@grantcohoe.com>
2026-05-21 22:08:03 -0400

    set defaults

--- modules/profiles/manifests/desktop/finder.pp
+++ modules/profiles/manifests/desktop/finder.pp
@@ -2,12 +2,16 @@
 # Tweaks to Finder and the macOS desktop environment.
 #
 class profiles::desktop::finder {
+
+  Cfcc::Osx_defaults {
+    user => lookup('camper_username'),
+    type => 'int',
+  }
+
   # Hide widgets from the desktop
   cfcc::osx_defaults { 'WindowManager-StandardHideWidgets':
     domain => 'com.apple.WindowManager',
     key    => 'StandardHideWidgets',
-    type   => 'int',
     value  => '1',
-    user   => lookup('camper_username'),
   }
 }
