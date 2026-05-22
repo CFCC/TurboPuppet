4ba6ab43829d9217a8f4d4ff41cd9b273586d9af
Grant Cohoe <grant@grantcohoe.com>
2026-05-21 22:20:37 -0400

    quotes

--- modules/cfcc/manifests/osx_defaults.pp
+++ modules/cfcc/manifests/osx_defaults.pp
@@ -23,14 +23,14 @@ define cfcc::osx_defaults (
     $type_flag = " -${type}"
 
     exec { "osx_defaults-${name}":
-      command => "${sudo_prefix}${defaults_bin} write ${domain} ${key}${type_flag} ${value}",
-      unless  => "/bin/sh -c \"${sudo_prefix}${defaults_bin} read ${domain} ${key} 2>/dev/null | /usr/bin/grep -qxF '${value}'\"",
+      command => "${sudo_prefix}${defaults_bin} write '${domain}' '${key}'${type_flag} ${value}",
+      unless  => "/bin/sh -c \"${sudo_prefix}${defaults_bin} read '${domain}' '${key}' 2>/dev/null | /usr/bin/grep -qxF '${value}'\"",
       path    => ['/usr/bin', '/bin'],
     }
   } else {
     exec { "osx_defaults-${name}":
-      command => "${sudo_prefix}${defaults_bin} delete ${domain} ${key}",
-      onlyif  => "/bin/sh -c \"${sudo_prefix}${defaults_bin} read ${domain} ${key} 2>/dev/null\"",
+      command => "${sudo_prefix}${defaults_bin} delete '${domain}' '${key}'",
+      onlyif  => "/bin/sh -c \"${sudo_prefix}${defaults_bin} read '${domain}' '${key}' 2>/dev/null\"",
       path    => ['/usr/bin', '/bin'],
     }
   }
